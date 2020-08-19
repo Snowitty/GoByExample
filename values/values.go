9516c22294dd46f2cccee972d3e728b5ddf9ab64
Snowitty <snowitty@163.com>
2020-08-19 09:24:09 +0800

    change

--- values/values.go
+++ values/values.go
@@ -1,17 +1,19 @@
 package main
 
+import "fmt"
 
-func main(){
-    // 字符串可以通过 `+` 连接。
-    fmt.Println("go" + "lang")
+func main() {
 
-    // 整数和浮点数
-    fmt.Println("1+1 =", 1+1)
-    fmt.Println("7.0/3.0 =", 7.0/3.0)
+	// 字符串可以通过 `+` 连接。
+	fmt.Println("go" + "lang")
 
-    // 布尔型，以及常见的布尔操作。
-    fmt.Println(true && false)
-    fmt.Println(true || false)
-    fmt.Println(!true)
+	// 整数和浮点数
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	// 布尔型，以及常见的布尔操作。
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!true)
 
 }
